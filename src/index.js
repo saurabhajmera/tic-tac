accd0c2ad63a63f44737662e2fafa71b663ba6aa
Saurabh Ajmera <sajmera@netflix.com>
2019-09-01 23:02:02 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,131 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import calculateWinner from "./winner-calculator";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    const onButtonClick = () => {
+        props.onClick();
+    };
+
+    return (
+        <button className="square"
+                onClick={onButtonClick}
+                >
+            {props.value}
+        </button>
+    );
+
+}
+
+function Board(props){
+
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         squares : Array(9).fill(null),
+    //         xIsNext: true
+    //     }
+    // }
+
+    const renderSquare = (i) => {
+        return <Square value={props.squares[i]} onClick={()=>props.handleClick(i)} />;
+    };
+
+
+
+        return (
+            <div>
+                <div className="board-row">
+                    {renderSquare(0)}
+                    {renderSquare(1)}
+                    {renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {renderSquare(3)}
+                    {renderSquare(4)}
+                    {renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {renderSquare(6)}
+                    {renderSquare(7)}
+                    {renderSquare(8)}
+                </div>
+            </div>
+        );
+
+
+}
+
+class Game extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            history:[{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true
+        }
+    }
+
+    jumpTo(step){
+        this.setState({
+           stepNumber: step,
+           xIsNext: (step % 2) === 0,
+        });
+    }
+
+    updateHistory = (i) => {
+        // console.log(i);
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if(calculateWinner(squares) || squares[i]){
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: [...history,{squares: squares}],
+            stepNumber:history.length,
+            xIsNext: !this.state.xIsNext});
+    };
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step,move) => {
+           const desc = move? 'Go to move #'+move:'Go to game start';
+           return (
+               <li key={move}> <button onClick={()=>this.jumpTo(move)}>{desc}</button></li>
+           );
+        });
+
+        let status;
+        if(winner){
+            status = 'Winner:' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X':'O');
+        }
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board squares={current.squares} xIsNext={this.state.xIsNext} handleClick={this.updateHistory}/>
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
